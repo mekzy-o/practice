6f08b43d3e19c74f92f7cf5ac77df6f7fad95958
Emeka <mekzycodes@gmail.com>
2019-03-22 15:42:43 +0100

    update changes to server folder and package.json

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import router from './routes';
+
+
+const app = express();
+
+//Activating morgan
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(router);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is live on PORT: ${port}`);
+});
+
+export default app;
---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+const router = express.Router();
+
+//Default route for API
+router.get('/', (req, res) => {
+    res.json({message: "Welcome to API of Politico"});
+    }
+);
+
+
+
+export default router;
