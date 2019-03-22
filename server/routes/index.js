40ff468abe8b32bc90b15e912889b1c1684b0e9a
Emeka <mekzycodes@gmail.com>
2019-03-22 16:56:32 +0100

    add eslint to lintup code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,10 @@
 import express from 'express';
 
-
 const router = express.Router();
 
-//Default route for API
+// Default route for API
 router.get('/', (req, res) => {
-    res.json({message: "Welcome to API of Politico"});
-    }
-);
-
-
+  res.json({ message: 'Welcome to API of Politico' });
+});
 
 export default router;
